c73afbcb21434eaaa1c2d494817267837723afed
Juan_Martinez <jmartinez304@mail.valenciacollege.edu>
2021-01-16 22:50:26 -0500

    first commit

---
+++ app/src/main/java/com/example/inventorymanagement/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.inventorymanagement
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<Button>(R.id.goToAddProduct).setOnClickListener {
+            startActivity(Intent(this, AddProductActivity::class.java))
+        }
+    }
+}
