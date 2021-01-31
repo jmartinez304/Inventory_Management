0e2cbec3a56d64db54789c48919fac97628b0744
Juan_Martinez <jmartinez304@mail.valenciacollege.edu>
2021-01-31 18:29:58 -0500

    Android Tutorials 5 & 6 changes

--- app/src/main/java/com/example/inventorymanagement/MainActivity.kt
+++ app/src/main/java/com/example/inventorymanagement/MainActivity.kt
@@ -11,6 +11,10 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,14 +23,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<Button>(R.id.goToAddProduct).setOnClickListener {
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
             startActivity(Intent(this, AddProductActivity::class.java))
         }
 
-        val preferences = getSharedPreferences("database", Context.MODE_PRIVATE)
-        val savedName = preferences.getString("savedProductName", "This value doesn't exist.")
-        d("juan", "saved message is: $savedName")
+//        val products = listOf("Building", "Car", "Truck", "Ubuntu Laptop", "Macbook Pro", "Google Home")
+        val products = listOf(
+                Product("iPod", "Juan", 2006, 260.59),
+                Product("Pixel", "John", 2009, 12.34),
+                Product("Mac", "Nathan", 2020, 4999.99),
+                Product("Monitor", "Hannah", 1999, 500.00),
+                Product("Kotlin", "Mel", 1776, 0.0),
+                Product("MacBook Pro", "Daniel Malone", 2020, 4399.99)
+        )
 
-        findViewById<TextView>(R.id.lastSavedProduct).text = savedName
+        var totalCost = 0.0
+
+        products.forEach {
+            findViewById<TextView>(R.id.productsTextView).append("${it.name} - ${it.owner} - ${it.yearPurchased} - $ ${it.cost} \n\n")
+            totalCost += it.cost
+        }
+
+        d("juan", "Total cost is $totalCost")
+
+        findViewById<TextView>(R.id.totalCostTextView).text = "$ $totalCost"
     }
 }
